a7b717d79f99da0dad201e4b481ac76e10b2957f
gabs1311 <gabs.antonovici@gmail.com>
2022-07-19 13:40:01 +0300

    continue

--- src/test/java/com/FinWorks/runners/CukesRunner.java
+++ src/test/java/com/FinWorks/runners/CukesRunner.java
@@ -13,7 +13,12 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/jsonReports/cucumber-reports.json"
         },
+
+
+
+
 
         features = "src/test/resources/features",
         glue="com/FinWorks/step_definitions",
